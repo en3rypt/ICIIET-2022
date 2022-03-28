4d38fa08e0465fa0d9fc09d393dbb037cf6a5049
20PW14 <76254926+karthiiii@users.noreply.github.com>
2022-03-29 02:50:24 +0530

    added new scroll js code

--- js/index.js
+++ js/index.js
@@ -13,3 +13,25 @@
 
 //     setTimeout('pageScroll()', scrollSpeed);
 // }
+
+// var startDelay = 1200;
+// var scrollSpeed = 1000; // if u make this large as 1800 or so, autoscroll is slow, making page task less heavy and voila! everything works as intended
+// setTimeout(function () {
+//     setInterval(function () {
+//         var autoScrollDiv = document.getElementById('article-container');
+//         autoScrollDiv.scrollTop = autoScrollDiv.scrollTop + 1;
+//         if (autoScrollDiv.scrollTop == (autoScrollDiv.scrollHeight - 320))
+//             autoScrollDiv.scrollTop = 0;
+//     }, scrollSpeed)
+// }, startDelay);
+
+// document.querySelectorAll('a').forEach(function (aTag) {
+//     aTag.onclick = function (event) {
+//         event.preventDefault()
+//         event.stopPropagation()
+//         var loc = aTag.href.substring(aTag.href.lastIndexOf('#') + 1)
+//         console.log(loc)
+//         if (loc) document.getElementById(loc).scrollIntoView({ behavior: 'smooth' });
+//     }
+// })
+
