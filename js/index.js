15bc931ff6387b466db9d3c60054667cc6a8eedc
20PW14 <76254926+karthiiii@users.noreply.github.com>
2022-03-28 18:34:31 +0530

    removed console.log from index.js

--- js/index.js
+++ js/index.js
@@ -7,10 +7,9 @@ var scrollSpeed = 18;
 setTimeout('pageScroll()', startDelay);
 function pageScroll() {
     var objDiv = document.getElementById('article-container');
-    console.log(objDiv.scrollTop, objDiv.scrollHeight);
     objDiv.scrollTop = objDiv.scrollTop + 1;
-    if (objDiv.scrollTop == (objDiv.scrollHeight - 352)) 
+    if (objDiv.scrollTop == (objDiv.scrollHeight - 352))
         objDiv.scrollTop = 0;
-    
+
     setTimeout('pageScroll()', scrollSpeed);
 }
